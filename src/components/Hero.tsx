bab939c8159b316c78cff3887532fa6854b6ab96
Hemit Patel <hemitvpatel@gmail.com>
2024-12-20 18:11:48 -0500

    added hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { GithubIcon, LinkedinIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const Hero = () => {
+  const router = useRouter();
+
+  const handleRedirect = (link: string) => {
+    router.push(link);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen relative">
+      {/* Name/Image */}
+      <Image
+        src="/HeroName.png"
+        alt="my name"
+        width={500}
+        height={0}
+      />
+
+      {/* Description */}
+      <div className="max-w-[700px] mt-7">
+        <p className="text-xl text-center font-medium">
+          an aspiring software engineer, focusing on beautiful designs, shipping impact, GTFOL
+        </p>
+      </div>
+
+      {/* Social Media Buttons */}
+      <div className="mt-4 space-x-2">
+        <button
+          onClick={() => handleRedirect("https://github.com/hemit99123")}
+          className="border-2 border-gray-500 rounded-full px-2 py-2"
+        >
+          <GithubIcon />
+        </button>
+        <button
+          onClick={() => handleRedirect("https://www.linkedin.com/in/hemit-patel-383ab3271/")}
+          className="border-2 border-gray-500 rounded-full px-2 py-2"
+        >
+          <LinkedinIcon />
+        </button>
+      </div>
+
+      {/* Goodies Section */}
+      <div className="cursor-pointer absolute bottom-16 flex flex-col items-center" onClick={() => handleRedirect("/#projects")}>
+        <p className="text-sm">down here for the goodies~</p>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width={50}
+          height={50}
+        >
+          <path
+            d="M12 5V19M12 19L6 13M12 19L18 13"
+            stroke="#000000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
