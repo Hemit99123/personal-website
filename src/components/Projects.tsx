57f8cff28af4d18b9a21623efe0daa2f5409629d
Hemit Patel <hemitvpatel@gmail.com>
2024-12-20 19:42:43 -0500

    moved the data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
+import projectData from '@/data/projects';
 
 const Projects = () => {
   // Define an array of project objects with width and height
-  const projectData = [
-    {
-      title: 'Talem',
-      imgSrc: '/projects-assets/talem.png',
-      hoverImgSrc: '/projects-assets/talem-hover.png',
-    },
-    {
-      title: 'DailySAT',
-      imgSrc: '/projects-assets/dailysat.png',
-      hoverImgSrc: '/projects-assets/dailysat-hover.png',
 
-    },
-    {
-      title: 'Trashify',
-      imgSrc: '/projects-assets/trashify.png',
-      hoverImgSrc: '/projects-assets/trashify-hover.png',
-    },
-    {
-      title: 'Everyone Classroom',
-      imgSrc: '/projects-assets/everyone-stem.png',
-      hoverImgSrc: '/projects-assets/everyone-stem-hover.png',
-      },
-  ];
 
   return (
     <div id="projects" className="mt-10 px-52 lg:px-30">
---
+++ src/data/projects.ts
@@ -0,0 +1,25 @@
+const projectData = [
+    {
+      title: 'Talem',
+      imgSrc: '/projects-assets/talem.png',
+      hoverImgSrc: '/projects-assets/talem-hover.png',
+    },
+    {
+      title: 'DailySAT',
+      imgSrc: '/projects-assets/dailysat.png',
+      hoverImgSrc: '/projects-assets/dailysat-hover.png',
+
+    },
+    {
+      title: 'Trashify',
+      imgSrc: '/projects-assets/trashify.png',
+      hoverImgSrc: '/projects-assets/trashify-hover.png',
+    },
+    {
+      title: 'Everyone Classroom',
+      imgSrc: '/projects-assets/everyone-stem.png',
+      hoverImgSrc: '/projects-assets/everyone-stem-hover.png',
+      },
+];
+
+export default projectData
