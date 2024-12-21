bddf1770b8a7d4716c724157da844f4f27a09052
Hemit Patel <hemitvpatel@gmail.com>
2024-12-20 21:17:13 -0500

    fixed ui, make it look more sigma

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <div id="projects" className="mt-10 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-52 mb-20">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-black mb-10">Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
         {/* Map through projectData and pass props to ProjectItem */}
         {projectData.map((project, index) => (
           <ProjectItem
