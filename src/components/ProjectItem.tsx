86f8700241febd2b0ea0b13ff574643cff72e97c
Hemit Patel <hemitvpatel@gmail.com>
2024-12-20 21:13:53 -0500

    made it responsiveness

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -14,17 +14,17 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ title, imgSrc, hoverImgSrc}) 
   return (
     <div className="cursor-pointer">
       <div
-        className="border-2 border-black flex justify-center items-center h-80 relative overflow-hidden"
+        className="border-2 border-black flex justify-center items-center relative overflow-hidden h-80 sm:h-96 md:h-[20rem] lg:h-[24rem] xl:h-[28rem]"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <img
           src={isHovered ? hoverImgSrc : imgSrc}
           alt={title}
-          className={`transition-all duration-300 ease-in-out ${isHovered ? 'object-cover w-full h-full' : `w-20 h-20`}`}
+          className={`transition-all duration-300 ease-in-out ${isHovered ? 'object-cover w-full h-full' : 'w-20 h-20 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 xl:w-56 xl:h-56'}`}
         />
       </div>
-      <p className="text-xl font-medium">{title}</p>
+      <p className="text-xl font-medium text-center">{title}</p>
     </div>
   );
 };
