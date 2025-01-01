3dbc3e1a7e5996f94cf442c2cd4b74c4499ebd97
Hemit Patel <hemitvpatel@gmail.com>
2025-01-01 09:06:23 -0500

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -6,7 +6,7 @@ const Experience = () => {
   return (
     <div className="mt-10 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-52 mb-16">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-black mb-4">Testimonials</h1>
-      <p className="text-gray-500 mb-8">People who think I&apos;m goated</p>
+      <p className="text-gray-500 mb-8">People who think my work is amazing </p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         {testimonials.map((item, index) => (
