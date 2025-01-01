84261c3fbdf42d79839bfc9fb685056d703cf48a
Hemit Patel <hemitvpatel@gmail.com>
2025-01-01 09:07:04 -0500

    removed emoticons

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
   return (
     <div className="mt-10 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-52 mb-16">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-black">My Experience</h1>
-      <p className="text-gray-500 mb-5">These are some cool things I&apos;m working/worked on :)</p>
+      <p className="text-gray-500 mb-5">These are some cool things I&apos;m working/worked on</p>
       <div className="space-y-6">
         {jobs.map((job, index) => (
             <Job 
