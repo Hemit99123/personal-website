a6af2599d22883fc9faf95a76994e0a11165c677
Hemit Patel <hemitvpatel@gmail.com>
2025-01-03 22:46:03 -0500

    format properly

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -10,12 +10,17 @@ const BlogSection = () => {
     const handleGoBlog = () => {
         router.push("https://hemitdev.wordpress.com/")
     }
-    
+
   return (
     <div className="flex flex-col items-center my-20">
         <h2 className="text-blue-500 font-bold text-3xl">My SWE Blog</h2>
         <p className="text-gray-500">I am also into blogging new technologies and learnings I find!</p>
-        <button onClick={handleGoBlog} className="bg-black text-white px-10 py-3 rounded-lg hover:bg-white hover:text-black duration-500 border-2 mt-2">Check it out 😪</button>
+        <button 
+            onClick={handleGoBlog} 
+            className="bg-black text-white px-10 py-3 rounded-lg hover:bg-white hover:text-black duration-500 border-2 mt-2"
+        >
+            Check it out 😪
+        </button>
     </div>  
   )
 }
